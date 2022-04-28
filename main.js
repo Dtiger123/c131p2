36ce362f83b5e1c27f99711c28e4b422f03dcd89
Dtiger123 <88206528+Dtiger123@users.noreply.github.com>
2022-04-27 20:15:36 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,12 +4,12 @@ function setup(){
     canvas.center();
 }
 function preload(){
-    img = loadImage("iphone-13-blue-select-2021.jpg");
+    img = loadImage("istockphoto-502706233-612x612.jpg");
 }
 function draw(){
     image(img,0,0,640,420);
     fill("#FF0000");
-    text("IPhone",45,75);
+    text("TV",45,75);
     noFill();
     stroke("#FF0000");
     rect(30,60,450,350);
